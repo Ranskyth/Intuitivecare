287962e8b6587af2e561f331d57df7143d555626
gabriel <ga.lima2205@gmail.com>
2025-03-30 16:28:26 -0300

    feat: create transform data

---
+++ teste2_tranformacaoDados/main.py
@@ -0,0 +1,34 @@
+from zipfile import ZipFile
+import tabula
+
+pageStart = 3
+pageEnd = 181
+
+def extract():
+    with ZipFile("Zip/Anexos.zip","r") as file:
+        file.extract("PDFs/Anexo_I.pdf")
+
+def readFile():
+    pdfs = tabula.read_pdf("PDFs/Anexo_I.pdf", pages=f"{pageStart}-{pageEnd}", multiple_tables=True)
+    isHeader = False
+    for pdf in pdfs:
+        
+        pdf.rename(columns={pdf.columns[1]:"RN(alteração)",pdf.columns[3]:"Seg. Odontológica",pdf.columns[4]:"Seg. Ambulatorial"},inplace=True)
+ 
+        if not isHeader:
+            pdf.to_csv("dados.csv", mode='a', index=False)
+            isHeader=True
+        else:
+            pdf.to_csv("dados.csv", header=False, mode='a', index=False)
+
+def compact():
+    with ZipFile("Teste_GabrielLimaSantana.zip", 'w') as comp:
+        comp.write("dados.csv")
+
+def main():
+    extract()
+    readFile()
+    compact()
+
+if __name__ == "main":
+    main()
